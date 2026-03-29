4c81750d14f5a615c9793c5d63a4f0e84cb96892
appyouz <dikpalkhatri7@gmail.com>
2026-03-29 21:43:29 +0545

    feat; implmeent the string comapare functionality - Can check if char press is correct or not and go back to if press   backspace

--- main.cpp
+++ main.cpp
@@ -1,45 +1,47 @@
 #include <cmath>
+#include <cstdio>
+#include <fstream>
 #include <functional>
-#include <ncurses.h>
 #include <iostream>
+#include <ncurses.h>
+#include <string>
 #include <thread>
-#include <utility>
 #include <future>
 #include <chrono>
 #include <unistd.h>
-#include <stdio.h>
 #include <future>
 
-/*
- * TODO:
- * Logic:
- * Initiate a timer and equate counter to that timer. The counter now will start the
- * tick tick and once the counter is up we store the char typed everytime a keystroke is
- * pressed and use that to calculate wpm along with the time (seconds)., the counter
- * exists sole purpose of the while loop base condition
- *
- *
- * 1. Use thread for it. 
- * AT one thread run the timer so it goes on once it starts.
- * New approach:
- * Run the user input in a thread and attach that thread to our main while loop
- *
- *
- * -(NOT POSSIBLE) Write two threads and call the use rinput thread to coutndown timer thread(TWO
- *   THREAD FUNCTIONS)
-* */
 bool countDown(int time);
-void takeUserInput(int &char_pressed){
-  int ch = getch();
+void takeUserInput(int &char_pressed, std::string lessonText, int& i){
   float time = 10.0;
-  // int ch = getch();
-// if ((ch = getch()) != KEY_F(1)) {
-  if(ch  != ERR){
-    char_pressed++;
-    // printw(" CHAR:%d", ch);
-    printw(keyname(ch));
+  // This block works but prints out lesson.size() time every letter. but char pressed
+  // and wpm is correct still. need to somehow not let it print lesson.size() for a
+  // single keystroke
+  // Okay writing ch = getch() fixed the lesson.size() times of a letter but char
+  // pressed is wrong now haha
+ int n = lessonText.length(); 
+    int ch;
+ch = getch();
+// for(int i = 0; i < n; i++){
+  if(i > n) return;
+    if(ch != ERR){
+    // {solved)TODO: logic for backspace and ch != lessonString[i]
+    if(ch == KEY_BACKSPACE){
+      i--;
+    }
+      if(ch == lessonText[i]){
+      ++char_pressed;
+      // printw("char_pressed: %d\n",++char_pressed);
+      printw(keyname(ch));
+      // printw("i: %d\n",i++);
+      i++;
+      }else if(ch != KEY_BACKSPACE && ch != lessonText[i]){
+      printw(keyname(ch));
+      i++;
+      // printw("i: %d\n",i++);
+      }
+    // }
   }
-
 }
 
 bool countDown(int time){
@@ -61,15 +63,22 @@ int main(){
   keypad(stdscr, TRUE);
   noecho();
 
+  // std::string lessonText {"The Lazy brown Fox Jump Over The Wall"};
+  std::string lessonText {"the man"};
+  
+  printw("%s", lessonText.data());
+  printw("\n");
+  
   int time = 10;
   int char_pressed = 0;
   int count = time;
+  int i = 0;
   std::future<bool> backgroundThread = std::async(std::launch::async,countDown,std::ref(time));
   std::future_status status;
 while(true){
-    std::thread t (takeUserInput,std::ref(char_pressed));
-    t.join();
-    // takeUserInput(char_pressed);
+    // std::thread t (takeUserInput,std::ref(char_pressed));
+    // t.join();
+    takeUserInput(char_pressed, lessonText, i);
     status = backgroundThread.wait_for(std::chrono::milliseconds(50));
     if(status == std::future_status::ready){
       // std::cout << "TIMES UP";
@@ -79,6 +88,7 @@ while(true){
   }
   int wpm = 0;
   wpm = static_cast<int>(std::round((char_pressed / 5.0) / (time / 60.0)));
+  printw("Character pressed: %d\n", char_pressed);
   printw("\nwpm:%d", wpm);
   refresh();
   // getch();
